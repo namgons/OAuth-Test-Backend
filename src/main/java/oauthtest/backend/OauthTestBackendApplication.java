27ee197a11e7f7d752a68c0a0ca20a927c51ffe1
이남곤 <nglee.dev@gmail.com>
2023-09-18 11:14:39 +0900

    env: 프로젝트 시작

---
+++ src/main/java/oauthtest/backend/OauthTestBackendApplication.java
@@ -0,0 +1,13 @@
+package oauthtest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthTestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oauthtest/backend/OauthTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package oauthtest.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
