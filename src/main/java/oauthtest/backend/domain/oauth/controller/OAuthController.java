ac26158500827d67c753a091624a5858d8e1e1a0
이남곤 <nglee.dev@gmail.com>
2023-09-18 22:55:04 +0900

    fix: PathVariable로 oauthServerType 파싱

--- src/main/java/oauthtest/backend/domain/oauth/controller/OAuthController.java
+++ src/main/java/oauthtest/backend/domain/oauth/controller/OAuthController.java
@@ -20,21 +20,17 @@ public class OAuthController {
     @GetMapping("/{oauthServerType}")
     public ResponseEntity<?> redirectAuthCodeRequestUrl(@PathVariable OAuthServerType oauthServerType) {
         String redirectUrl = oauthService.getAuthCodeRequestUrl(oauthServerType);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setLocation(URI.create(redirectUrl));
         return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
     }
 
-    // 추가
-    @GetMapping("/kakao/login")
+    @GetMapping("/{oauthServerType}/login")
     ResponseEntity<Long> login(
-//            @PathVariable OAuthServerType oauthServerType,
-            @RequestParam("code") String code
-    ) {
-        System.out.println("OAuthController.login");
-//        System.out.println(oauthServerType);
-        Long login = oauthService.login(OAuthServerType.KAKAO, code);
+            @PathVariable OAuthServerType oauthServerType,
+            @RequestParam("code") String code) {
+        System.out.println(oauthServerType);
+        Long login = oauthService.login(oauthServerType, code);
         return ResponseEntity.ok(login);
     }
 }
